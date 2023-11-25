19064b21be9ed1f448d283b3936da3b186a48d49
Porcelanic <porcelanic0907@gmail.com>
2023-11-25 04:47:56 -0500

    Guardado de estampados
    Se guardan los estampados publicados por los artistas en la carpeta public dentro de la carpeta 'Estampados'

--- frontend/src/Pages/FormEstampado.jsx
+++ frontend/src/Pages/FormEstampado.jsx
@@ -7,50 +7,91 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/esm/Container";
 
 export default function FormEstampado() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      fetch("http://localhost:4000/image", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          // Handle response from the server
+          if (response.ok) {
+            return response.json(); // Parse response JSON if status is OK
+          }
+          throw new Error("File upload failed");
+        })
+        .then((data) => {
+          // Handle the file path returned from the server
+          console.log("File uploaded successfully! Path:", data.filePath);
+          // Use the path as needed in your application
+        })
+        .catch((error) => {
+          // Handle errors
+          console.error("Error uploading file:", error);
+        });
+    }
+  };
+
   return (
     <>
       <Header />
-      <Form onSubmit={null}>
-        <Row className="d-flex justify-content-around mt-5 pt-5">
-          <Col className="recuadro bordered p-5" md={{ span: 4, offset: 4 }}>
-            <h2 className="text-center mb-5">Publica tu estampado</h2>
-            <Form.Group controlId="formFile" className="mb-3">
-              <Form.Label>Publica tu estampado</Form.Label>
-              <Form.Control type="file" />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                placeholder="Descripcion"
-                maxLength={45}
-              />
-            </Form.Group>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Row className="d-flex justify-content-around mt-5 pt-5">
+            <Col className="recuadro bordered p-5" md={{ span: 6, offset: 4 }}>
+              <h2 className="text-center mb-5">Publica tu estampado</h2>
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Publica tu estampado</Form.Label>
+                <Form.Control type="file" onChange={handleFileChange} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Nombre"
+                  maxLength={20}
+                />
+                <Form.Text>Ponle un nombre unico a tu estampado</Form.Text>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="Categoria">
-              <Form.Select data-testid="Categoria">
-                <option value="">Talla</option>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-              </Form.Select>
-            </Form.Group>
-            <div className="d-grid ">
-              <Button
-                variant="outline-light"
-                type="submit"
-                className="ms-3 d-grid"
-                size="md"
-              >
-                Publicar
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </Form>
+              <Form.Group className="mb-3" controlId="Categoria">
+                <Form.Select data-testid="Categoria">
+                  <option value="">Categoria</option>
+                  <option value="Abstracto">Abstracto</option>
+                  <option value="Naturales">Naturales</option>
+                  <option value="Retro">Retro</option>
+                  <option value="Tematicos">Temáticos</option>
+                  <option value="Grafico">Otro</option>
+                </Form.Select>
+                <Form.Text>Con que categoria crees que se identifica tu estampado?</Form.Text>
+              </Form.Group>
+              <div className="d-grid ">
+                <Button
+                  variant="outline-light"
+                  type="submit"
+                  className="ms-3 d-grid"
+                  size="md"
+                >
+                  Publicar
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
       <ThemeSwitcher />
       <Footer />
     </>
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -10,32 +10,30 @@ import {
 import {
   createDireccion,
   getDireccion,
-} from '../controllers/direccionDAO.controller.js';
-import { 
-  createPago 
-} from "../controllers/pagoDAO.controller.js";
+} from "../controllers/direccionDAO.controller.js";
+import { createPago } from "../controllers/pagoDAO.controller.js";
+import {
+  saveImage
+} from "../controllers/imagenDAO.controller.js";
 
 const router = Router();
 
 // Rutas para las consultas en cliente
-
 router.post("/clients", createCliente);
-
 router.get("/clients/:email", getCliente);
 
 // Rutas para las consultas en artistas
-
 router.post("/artists", createArtista);
-
 router.get("/artists/:email", getArtista);
 
 // Rutas para insertar direccion
-
 router.post("/addresses", createDireccion);
 router.get("/addresses/:email", getDireccion);
 
 // Rutas para pago
+router.post("/payment", createPago);
 
-router.post("/payment", createPago)
+// Rutas estampado
+router.post("/image", saveImage)
 
 export default router;
