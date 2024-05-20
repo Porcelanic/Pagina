7ca094ceca24f3cf7964d41412a102d1f4d57e09
Porcelanic <porcelanic0907@gmail.com>
2024-05-19 19:14:12 -0500

    Image UPLOAD fix
    cuando se intentaba subir un estampado salía un error debido a que había un path incorrecto en el backend.

--- backend/src/controllers/imagenDAO.controller.js
+++ backend/src/controllers/imagenDAO.controller.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "frontend/public/Estampados"); // Define the destination folder for uploads
+    cb(null, "../frontend/public/Estampados"); // Define the destination folder for uploads
   },
   filename: (req, file, cb) => {
     cb(
