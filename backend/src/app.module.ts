d0b4d4a0b8a4c4fd439eec4c3ba27419d2d608ed
Manuel Gonzalez <porcelanic0907@gmail.com>
2025-05-20 21:55:49 -0500

    fixed a relative route

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { ClienteModule } from './Modelos/cliente/cliente.module';
 import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
 import config from './config';
-import { DatabaseModule } from './dataBase/database.module';
+import { DatabaseModule } from './database/database.module';
 import { EstampadoModule } from './Modelos/estampado/estampado.module';
 import { ArtistaModule } from './Modelos/artista/artista.module';
 import { MaterialModule } from './Modelos/material/material.module';
